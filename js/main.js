50f2b43a72c1a95078d112b85fad31f4fe5fb917
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2022-10-09 20:30:30 -0300

    Pegando valores dos inputs do formulario (item e quantidade do item que deve colocar na mochila) e criando items de lista com esses valores através do JavaScript

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 const form = document.getElementById('novoItem')
 
+const lista = document.getElementById('lista')
+
 form.addEventListener('submit', event => {
   event.preventDefault()
-  event.target.elements['nome'].value
-  event.target.elements['quantidade']
+  const nome = event.target.elements['nome'].value
+  const quantidade = event.target.elements['quantidade'].value
+  criaElemento(nome, quantidade)
 })
+
+function criaElemento(nome, quantidade) {
+  const novoItem = document.createElement('li')
+  novoItem.classList.add('item')
+
+  const numeroItem = document.createElement('strong')
+  numeroItem.innerHTML = quantidade
+
+  novoItem.appendChild(numeroItem)
+  novoItem.innerHTML += nome
+
+  lista.appendChild(novoItem)
+}
