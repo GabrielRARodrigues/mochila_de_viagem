aa2fd5dcce15b0eacc7e4b568ecc293e55e6fbcc
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2022-10-11 20:07:57 -0300

    Refatorando o código para deixalo mais funcional, e adicinando os item da lista utilizando os dados armazenados no local storage

--- js/main.js
+++ js/main.js
@@ -2,22 +2,40 @@ const form = document.getElementById('novoItem')
 
 const lista = document.getElementById('lista')
 
-form.addEventListener('submit', event => {
-  event.preventDefault()
-  const nome = event.target.elements['nome'].value
-  const quantidade = event.target.elements['quantidade'].value
-  criaElemento(nome, quantidade)
+const items = JSON.parse(localStorage.getItem('items')) || []
+
+items.forEach(elemento => {
+  criaElemento(elemento)
 })
 
-function criaElemento(nome, quantidade) {
+form.addEventListener('submit', event => {
+  event.preventDefault()
+  const nome = event.target.elements['nome']
+  const quantidade = event.target.elements['quantidade']
+
+  const itemAtual = {
+    nome: nome.value,
+    quantidade: quantidade.value
+  }
+  criaElemento(itemAtual)
+
+  items.push(itemAtual)
+
+  localStorage.setItem('items', JSON.stringify(items))
+
+  nome.value = ''
+  quantidade.value = ''
+})
+
+function criaElemento(item) {
   const novoItem = document.createElement('li')
   novoItem.classList.add('item')
 
   const numeroItem = document.createElement('strong')
-  numeroItem.innerHTML = quantidade
+  numeroItem.innerHTML = item.quantidade
 
   novoItem.appendChild(numeroItem)
-  novoItem.innerHTML += nome
+  novoItem.innerHTML += item.nome
 
   lista.appendChild(novoItem)
 }
